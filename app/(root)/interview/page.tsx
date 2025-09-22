e89b8981c56b7cc6368bb1ca85895ec93a8f6c89
Kodenaime <creativedannyk@gmail.com>
2025-09-22 15:51:23 +0100

    interview generatio UI finished

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,19 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+      <h3>Generate Interview</h3>
+
+      <Agent 
+        userName="You"
+        userId="user1"
+        type="generate"
+      />
+      
+    </>
+  )
+}
+
+export default Page
